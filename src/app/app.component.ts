da4486d60ac1975c0bde5dc053495005f9c99c79
fitri-a <fitripasaribu102@gmail.com>
2026-05-03 17:10:52 +0700

    refactor: penyesuaian component dengan  appId

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, ToastController, IonRouterOutlet } from '@ionic/angular';
+import { App } from '@capacitor/app';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,41 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  @ViewChild(IonRouterOutlet, { static: true }) routerOutlet?: IonRouterOutlet;
+  private lastTimeBackPress = 0;
+  private timePeriodToExit = 2000;
+
+  constructor(
+    private platform: Platform,
+    private toastController: ToastController,
+    private router: Router
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.platform.backButton.subscribeWithPriority(10, async (processNextHandler) => {
+        if (!this.routerOutlet?.canGoBack() || this.router.url === '/home' || this.router.url === '/login') {
+          if (new Date().getTime() - this.lastTimeBackPress < this.timePeriodToExit) {
+            App.exitApp();
+          } else {
+            this.showToast();
+            this.lastTimeBackPress = new Date().getTime();
+          }
+        } else {
+          processNextHandler();
+        }
+      });
+    });
+  }
+
+  async showToast() {
+    const toast = await this.toastController.create({
+      message: 'Ketuk sekali lagi untuk keluar',
+      duration: 2000,
+      position: 'bottom'
+    });
+    await toast.present();
+  }
 }
