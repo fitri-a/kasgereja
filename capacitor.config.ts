4ebbf51489392a653cd88f010f80a41f823f0162
fitri-a <fitripasaribu102@gmail.com>
2026-05-03 17:31:16 +0700

    feat: icon app &  splash screen android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.kasgereja.app',
   appName: 'kasgitgereja',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#f4f5f8",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
-
 export default config;
